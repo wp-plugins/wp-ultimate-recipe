512ec53beabdcfb1666d46513a5e58361e595d61
BrechtVds <BrechtVds@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-21 06:46:15 +0000

    Clean up svn
    git-svn-id: https://plugins.svn.wordpress.org/wp-ultimate-recipe/trunk@952016 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- helper/custom_style_public.css.php
+++
@@ -1,4 +0,0 @@
-<?php
-header('Content-type: text/css');
-
-echo $this->option('custom_code_public_css', '');
--- js/admin_settings.js
+++
@@ -1,7 +0,0 @@
-jQuery(document).ready(function() {
-    if(typeof ajaxurl === 'undefined'){
-        var ajaxurl = wpurp_ajax.ajaxurl;
-    };
-
-    alert(ajaxurl);
-});
