801f487b8a9699672ccfb1514ff615ec81defa4b
BrechtVds <BrechtVds@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-28 21:08:14 +0000

    Version 2.1.3b
    git-svn-id: https://plugins.svn.wordpress.org/wp-ultimate-recipe/trunk@1034717 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-ultimate-recipe.php
+++ wp-ultimate-recipe.php
@@ -3,12 +3,12 @@
 Plugin Name: WP Ultimate Recipe
 Plugin URI: http://www.wpultimaterecipe.com
 Description: Everything a Food Blog needs. Beautiful SEO friendly recipes, print versions, visitor interaction, ...
-Version: 2.1.3
+Version: 2.1.3b
 Author: Bootstrapped Ventures
 Author URI: http://bootstrapped.ventures
 License: GPLv2
 */
-define( 'WPURP_VERSION', '2.1.3' );
+define( 'WPURP_VERSION', '2.1.3b' );
 
 class WPUltimateRecipe {
 
